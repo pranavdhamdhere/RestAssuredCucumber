b854a2d092a943c751fcd216adcf20396292bbec
pranav <pranavdhamdhere97@gmail.com>
2022-12-28 09:47:21 +0530

    minor changes

--- CucumberBDDRestAAssuredDemo-main/src/test/java/runner/TestRunner.java
+++ CucumberBDDRestAAssuredDemo-main/src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/test/resources/featureFiles"},
         glue = {"step.definitions"},
-        monochrome = false,
+        monochrome = true,
         dryRun = false,
         plugin = {
                 "json:build/cucumber-reports/cucumber.json",
